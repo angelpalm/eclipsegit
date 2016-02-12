64da2c92ef8f89a7e3b3e857ce07356ac3b4e4e5
atranrp <atran@resultspositive.com>
2016-02-11 23:38:06 -0600

    Initial Check In

---
+++ EclipseGitHub/src/CalculatorLibrary.java
@@ -0,0 +1,16 @@
+
+
+	public class CalculatorLibrary {
+
+		public static void main(String[] args) {
+		}
+
+			
+			int add (int num1, int num2)
+			
+			{
+				return num1 + num2;
+			}
+			
+
+		}
